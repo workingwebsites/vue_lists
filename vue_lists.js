7eeade7069670394bac97a26cfb7068d9089bada
Lisa Armstrong <lisa@workingwebsites.ca>
2019-05-03 11:03:07 -0700

    Commit

---
+++ vue_lists.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        countries_list: [
+            { "name": "Canada", "code": "CAN" },
+            { "name": "Mexico", "code": "MEX" },
+            { "name": "United States of America", "code": "USA" }
+        ],
+    }
+})
