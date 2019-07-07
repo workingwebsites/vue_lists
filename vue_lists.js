465bed985580533f5ad44bc95428d820410ae78a
Lisa Armstrong <lisa@workingwebsites.ca>
2019-07-07 16:50:21 -0700

    Final version

--- vue_lists.js
+++ vue_lists.js
@@ -1,10 +1,18 @@
 var app = new Vue({
     el: '#app',
     data: {
-        countries_list: [
-            { "name": "Canada", "code": "CAN" },
-            { "name": "Mexico", "code": "MEX" },
-            { "name": "United States of America", "code": "USA" }
-        ],
+        countries_list: [],
+    },
+
+    mounted() {
+        axios
+            .get('https://restcountries.eu/rest/v2/region/americas?fields=name;capital;flag;population')
+            .then(response => (
+                this.countries_list = response.data
+                ))
+            .catch(error =>(
+                console.log(error)
+                ));
     }
+
 })
